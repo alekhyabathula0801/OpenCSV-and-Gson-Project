934c1310d84b1ebd4da5f4850fdcd97d7be53ab4
arun kumar <alekhya0801@gmail.com>
2020-05-07 15:05:32 +0530

    Refracted code using lists

--- src/main/java/com/opencsvandgsonproject/OpenCSVReader.java
+++ src/main/java/com/opencsvandgsonproject/OpenCSVReader.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class OpenCSVReader {
     private static final String SAMPLE_CSV_FILE_PATH = "C:\\Users\\arun kumar\\IdeaProjects\\OpenCSVandGsonProject\\src\\main\\resources\\demo.csv";
@@ -14,12 +15,12 @@ public class OpenCSVReader {
             Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
             CSVReader csvReader = new CSVReader(reader);
         )    {
-                String[] nextRecord;
-                while ((nextRecord = csvReader.readNext()) != null) {
-                System.out.println("Name : "+nextRecord[0]);
-                System.out.println("Email : "+nextRecord[1]);
-                System.out.println("Phone Number : "+nextRecord[2]);
-                System.out.println("Country : "+nextRecord[3]);
+                List<String[]> records = csvReader.readAll();
+                for ( String[] record : records) {
+                System.out.println("Name : "+record[0]);
+                System.out.println("Email : "+record[1]);
+                System.out.println("Phone Number : "+record[2]);
+                System.out.println("Country : "+record[3]);
                 System.out.println("---------------------------");
             }
         } catch (IOException e) {
